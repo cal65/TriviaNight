4b950111541cc1fe2c16eb0c27308536bff4f841
Cal Lee <cal.christopheralee@gmail.com>
2020-09-08 00:25:59 -0400

    feat: stood up sqlite db
    Can input questions, can print them all back out

--- load_answers.py
+++ load_answers.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from flask_sqlalchemy import SQLAlchemy
 import logging
+from db import Questions, Responses
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__) 
@@ -15,52 +16,43 @@ def welcome():
 
 @app.route("/")
 def home():
-  return render_template("base.html")
+  	return render_template("base.html")
 
-@app.route('/json-example', methods=['POST']) #GET requests will be blocked
-def json_example():
-    return 'Todo...'
+@app.route('/return-all')
+def return_all():
+	all_questions = Questions.query.all()
+	return '\n'.join([q.question for q in all_questions])    
 
-#res = requests.post('http://127.0.0.1:9999/create-question/s9x93/what-is-the-country-code-of-switzerland', 
-#	json={"question_id":"s9x93", "round_id":"hgj394zl02", "question": "what is the country code of switzerland?",
-#	"author_id": "n92ne8s0x1"})
+@app.route('/<user_id>')
+def user(user_id):
+	return f'<h1>{user_id}</h1>'
 
-@app.route("/create-question/", methods=['POST', 'GET'])
+
+@app.route("/create-question", methods=['POST', 'GET'])
 def add_question():
-	content = request.get_json()
-	print (content['question'], flush=True)
-	question = content['question']
-	name = content['name']
-	#return str(question_id)
-	return (f'The question is {question} from {name}')
+	if request.method == 'POST':
+		print ("HI", flush=True)
+		content = request.get_json(force=True)
+		question = content['question']
+		author_id = content['author_id']
+		question_id = content['question_id']
+		round_id = content['round_id']
+		new_question = Questions(question_id=question_id, 
+			question=question, 
+			author_id=author_id,
+			round_id=round_id,
+			)
+		db.session.add(new_question)
+		db.session.commit()
+		return author_id + ": " + question
+	else:
+		return 'Sorry' + str(request)
 
 
-class questions(db.Model):
-   round_id = db.Column('round_id', db.String(8), primary_key = True)
-   question_id = db.Column(db.String(8), unique = True)
-   question = db.Column(db.String(2000))  
-   author_id = db.Column(db.String(8))
 
-class responses(db.Model):
-   response_id = db.Column('response_id', db.Integer, primary_key = True)
-   session_id = db.Column(db.String(8))
-   question_id = db.Column(db.String(8))  
-   team_id = db.Column(db.String(8))
-   round_id = db.Column(db.String(10))
-   response = db.Column(db.String(1000))
+db.create_all()
+db.session.commit()
 
-class answers(db.Model):
-   answer_id = db.Column('answer_id', db.Integer, primary_key = True)
-   question_id = db.Column(db.String(8), unique = False)
-   answer = db.Column(db.String(100))
 
-class game(db.Model):
-	gameid = db.Column('game_id', db.String, primary_key = True)
-
-def __init__(self, name, city, addr,pin):
-   self.name = name
-   self.city = city
-   self.addr = addr
-   self.pin = pin
 
 app.run(port=9999)
